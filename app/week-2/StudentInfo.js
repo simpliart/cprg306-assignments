5425ee63dc8a0a3f95459bf7b4f183ad4c925b37
simpliart <justinrdavis10@gmail.com>
2026-01-24 00:04:35 -0700

    add week-2 assignment files

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+  return (
+    <div>
+      <p>Justin Davis</p>
+      <p>
+         <Link href="https://github.com/simpliart/cprg306-assignments">My GitHub Repository</Link>
+      </p>
+    </div>
+  )
+}
