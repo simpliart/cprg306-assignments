e603c0dad50852db49251bbb1c8086360665fe9e
simpliart <justinrdavis10@gmail.com>
2026-01-31 00:03:37 -0700

    fixed spacing

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2,">Week 2</Link>
-        <Link href="/ week-3">Week 3</Link>
+        <Link href="/week-2">Week 2,</Link>
+        <Link href="/week-3"> Week 3</Link>
       </p>
     </main>
   );
