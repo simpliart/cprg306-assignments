10e17bc2c27fcb8102b34354e426e7e17199d381
simpliart <justinrdavis10@gmail.com>
2026-02-13 20:43:51 -0700

    added week 5 link

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Page() {
       <p>
         <Link href="/week-2">Week 2,</Link>
         <Link href="/week-3"> Week 3,</Link>
-        <Link href="/week-4"> Week 4</Link>
+        <Link href="/week-4"> Week 4,</Link>
+        <Link href="/week-5"> Week 5</Link>
       </p>
     </main>
   );
